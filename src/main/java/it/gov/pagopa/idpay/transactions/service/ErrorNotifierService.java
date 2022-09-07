90e87a0bbd2033e82f1b5ec50d3910ddf06b4710
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2022-09-07 11:50:25 +0200

    Helm chart update (#9)
    * helm chart update
    
    * sending server type when errors
    
    * probe config
    
    * extended helm upgrade timeout
    
    Co-authored-by: anttorre <antonio.torre@everis.com>

--- src/main/java/it/gov/pagopa/idpay/transactions/service/ErrorNotifierService.java
+++ src/main/java/it/gov/pagopa/idpay/transactions/service/ErrorNotifierService.java
@@ -4,5 +4,5 @@ import org.springframework.messaging.Message;
 
 public interface ErrorNotifierService {
     void notifyTransaction(Message<?> message, String description, boolean retryable, Throwable exception);
-    void notify(String srcServer, String srcTopic, Message<?> message, String description, boolean retryable, Throwable exception);
+    void notify(String srcType, String srcServer, String srcTopic, Message<?> message, String description, boolean retryable, Throwable exception);
 }
