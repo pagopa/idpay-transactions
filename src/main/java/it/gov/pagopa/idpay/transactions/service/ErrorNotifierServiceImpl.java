4ead135b7bf321886bd73afb0c6c783ae897fd90
anttorre <antonio.torre@everis.com>
2022-09-07 10:01:44 +0200

    sending server type when errors

--- src/main/java/it/gov/pagopa/idpay/transactions/service/ErrorNotifierServiceImpl.java
+++ src/main/java/it/gov/pagopa/idpay/transactions/service/ErrorNotifierServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class ErrorNotifierServiceImpl implements ErrorNotifierService {
 
+    public static final String ERROR_MSG_HEADER_SRC_TYPE = "srcType";
     public static final String ERROR_MSG_HEADER_SRC_SERVER = "srcServer";
     public static final String ERROR_MSG_HEADER_SRC_TOPIC = "srcTopic";
     public static final String ERROR_MSG_HEADER_DESCRIPTION = "description";
@@ -20,28 +21,32 @@ public class ErrorNotifierServiceImpl implements ErrorNotifierService {
 
     private final StreamBridge streamBridge;
 
+    private final String trxMessagingServiceType;
     private final String trxServer;
     private final String trxTopic;
 
     public ErrorNotifierServiceImpl(StreamBridge streamBridge,
 
+                                    @Value("${spring.cloud.stream.binders.kafka-transactions.type}") String trxMessagingServiceType,
                                     @Value("${spring.cloud.stream.binders.kafka-transactions.environment.spring.cloud.stream.kafka.binder.brokers}") String trxServer,
                                     @Value("${spring.cloud.stream.bindings.rewardTrxConsumer-in-0.destination}") String trxTopic) {
         this.streamBridge = streamBridge;
 
+        this.trxMessagingServiceType = trxMessagingServiceType;
         this.trxServer = trxServer;
         this.trxTopic = trxTopic;
     }
 
     @Override
     public void notifyTransaction(Message<?> message, String description, boolean retryable, Throwable exception) {
-        notify(trxServer, trxTopic, message, description, retryable, exception);
+        notify(trxMessagingServiceType, trxServer, trxTopic, message, description, retryable, exception);
     }
 
     @Override
-    public void notify(String srcServer, String srcTopic, Message<?> message, String description, boolean retryable, Throwable exception) {
+    public void notify(String srcType, String srcServer, String srcTopic, Message<?> message, String description, boolean retryable, Throwable exception) {
         log.info("[ERROR_NOTIFIER] notifying error: {}", description, exception);
         final MessageBuilder<?> errorMessage = MessageBuilder.fromMessage(message)
+                .setHeader(ERROR_MSG_HEADER_SRC_TYPE, srcType)
                 .setHeader(ERROR_MSG_HEADER_SRC_SERVER, srcServer)
                 .setHeader(ERROR_MSG_HEADER_SRC_TOPIC, srcTopic)
                 .setHeader(ERROR_MSG_HEADER_DESCRIPTION, description)
