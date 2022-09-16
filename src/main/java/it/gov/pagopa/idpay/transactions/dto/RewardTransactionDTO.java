6e02b6f6121c7e40b50aded3dbc4583fa59e3727
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2022-09-16 17:06:02 +0200

    aligned transaction models (#12)
    * aligned transaction models
    
    * renamed transactions fields
    
    Co-authored-by: anttorre <antonio.torre@everis.com>

--- src/main/java/it/gov/pagopa/idpay/transactions/dto/RewardTransactionDTO.java
+++ src/main/java/it/gov/pagopa/idpay/transactions/dto/RewardTransactionDTO.java
@@ -1,5 +1,7 @@
 package it.gov.pagopa.idpay.transactions.dto;
 
+import it.gov.pagopa.idpay.transactions.model.RefundInfo;
+import it.gov.pagopa.idpay.transactions.model.Reward;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,56 +19,39 @@ import java.util.Map;
 @Builder
 public class RewardTransactionDTO {
 
+    private String id;
+    private String userId;
     private String idTrxAcquirer;
-
     private String acquirerCode;
-
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private OffsetDateTime trxDate;
-
     private String hpan;
-
     private String operationType;
-
     private String circuitType;
-
     private String idTrxIssuer;
-
     private String correlationId;
-
     private BigDecimal amount;
-
     private String amountCurrency;
-
     private String mcc;
-
     private String acquirerId;
-
     private String merchantId;
-
     private String terminalId;
-
     private String bin;
-
     private String senderCode;
-
     private String fiscalCode;
-
     private String vat;
-
     private String posType;
-
     private String par;
-
     private String status;
-
     private List<String> rejectionReasons;
-
     private Map<String, List<String>> initiativeRejectionReasons;
-
     private List<String> initiatives;
+    private Map<String, Reward> rewards;
 
-    private Map<String,BigDecimal> rewards;
+    private String operationTypeTranscoded;
+    private BigDecimal effectiveAmount;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private OffsetDateTime trxChargeDate;
+    private RefundInfo refundInfo;
 
-    String userId;
 }
