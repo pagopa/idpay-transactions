3e489beb35db9f13a567ba99539d3eed8ef7a2c2
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2023-02-07 18:22:21 +0100

    IDP-273 fixConsumerKafkaTimeout (#48)
    * IDP-273 fixConsumerKafkaTimeout
    
    * Fix Kafka metadata timeout

--- src/test/java/it/gov/pagopa/idpay/transactions/BaseIntegrationTest.java
+++ src/test/java/it/gov/pagopa/idpay/transactions/BaseIntegrationTest.java
@@ -92,6 +92,10 @@ import static org.awaitility.Awaitility.await;
 @AutoConfigureDataMongo
 @AutoConfigureWebTestClient
 public abstract class BaseIntegrationTest {
+
+    @Value("${spring.application.name}")
+    public String applicationName;
+
     @Autowired
     protected EmbeddedKafkaBroker kafkaBroker;
     @Autowired
@@ -235,7 +239,7 @@ public abstract class BaseIntegrationTest {
     private int totaleMessageSentCounter =0;
     protected void publishIntoEmbeddedKafka(String topic, Iterable<Header> headers, String key, String payload) {
         final RecordHeader retryHeader = new RecordHeader("RETRY", "1".getBytes(StandardCharsets.UTF_8));
-        final RecordHeader applicationNameHeader = new RecordHeader(ErrorNotifierServiceImpl.ERROR_MSG_HEADER_APPLICATION_NAME, "idpay-transactions".getBytes(StandardCharsets.UTF_8));
+        final RecordHeader applicationNameHeader = new RecordHeader(ErrorNotifierServiceImpl.ERROR_MSG_HEADER_APPLICATION_NAME, applicationName.getBytes(StandardCharsets.UTF_8));
 
         AtomicBoolean containAppNameHeader = new AtomicBoolean(false);
         if(headers!= null){
@@ -345,7 +349,7 @@ public abstract class BaseIntegrationTest {
 
     protected void checkErrorMessageHeaders(String srcTopic, String group, ConsumerRecord<String, String> errorMessage, String errorDescription, String expectedPayload, boolean expectRetryHeader, boolean expectedAppNameHeader) {
         if(expectedAppNameHeader) {
-            Assertions.assertEquals("idpay-transactions", TestUtils.getHeaderValue(errorMessage, ErrorNotifierServiceImpl.ERROR_MSG_HEADER_APPLICATION_NAME));
+            Assertions.assertEquals(applicationName, TestUtils.getHeaderValue(errorMessage, ErrorNotifierServiceImpl.ERROR_MSG_HEADER_APPLICATION_NAME));
         }
         Assertions.assertEquals(group, TestUtils.getHeaderValue(errorMessage, ErrorNotifierServiceImpl.ERROR_MSG_HEADER_GROUP));
         Assertions.assertEquals("kafka", TestUtils.getHeaderValue(errorMessage, ErrorNotifierServiceImpl.ERROR_MSG_HEADER_SRC_TYPE));
