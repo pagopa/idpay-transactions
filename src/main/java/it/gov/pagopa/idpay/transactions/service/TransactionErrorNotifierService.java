45433778d1ad69e14bae1898d35c0a8e559e4524
DanieleRanaldo <Daniele.Ranaldo@nttdata.com>
2024-03-28 11:08:43 +0100

    [2IDP-2365] added configuration class, modified method and updated junit

--- src/main/java/it/gov/pagopa/idpay/transactions/service/TransactionErrorNotifierService.java
+++ src/main/java/it/gov/pagopa/idpay/transactions/service/TransactionErrorNotifierService.java
@@ -1,10 +1,10 @@
 package it.gov.pagopa.idpay.transactions.service;
 
+import it.gov.pagopa.idpay.transactions.config.KafkaConfiguration;
 import org.springframework.messaging.Message;
 
 public interface TransactionErrorNotifierService {
     void notifyTransaction(Message<?> message, String description, boolean retryable, Throwable exception);
     void notifyTransactionCommands(Message<String> message, String description, boolean retryable, Throwable exception);
-    @SuppressWarnings("squid:S00107") // suppressing too many parameters alert
-    void notify(String srcType, String srcServer, String srcTopic, String group, Message<?> message, String description, boolean retryable, boolean resendApplication, Throwable exception);
+    void notify(KafkaConfiguration.BaseKafkaInfoDTO baseKafkaInfoDTO ,Message<?> message, String description, boolean retryable, boolean resendApplication, Throwable exception);
 }
