9075d37c9332a5890fc4dec5d96c4dda471b41b9
michael11barone <Michael.Barone@nttdata.com>
2025-09-25 09:51:35 +0200

    Removed unused import

--- src/test/java/it/gov/pagopa/idpay/transactions/controller/PointOfSaleTransactionControllerImplTest.java
+++ src/test/java/it/gov/pagopa/idpay/transactions/controller/PointOfSaleTransactionControllerImplTest.java
@@ -18,7 +18,6 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
