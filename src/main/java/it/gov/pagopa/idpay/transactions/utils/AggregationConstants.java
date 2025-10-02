b368875b7d8317a4300d4b9649a30bccae37875a
fedcurciorubertini <federico.curciorubertini@nttdata.com>
2025-10-02 10:51:31 +0200

    fix: updated product sorting (#207)

--- src/main/java/it/gov/pagopa/idpay/transactions/utils/AggregationConstants.java
+++ src/main/java/it/gov/pagopa/idpay/transactions/utils/AggregationConstants.java
@@ -5,8 +5,7 @@ public class AggregationConstants {
   private AggregationConstants(){}
 
   public static final String FIELD_STATUS = "status";
-  public static final String FIELD_PRODUCT_CATEGORY = "additionalProperties.productCategory";
-  public static final String FIELD_PRODUCT_CATEGORY_IT = "categoryIt";
+  public static final String FIELD_PRODUCT_NAME = "additionalProperties.productName";
   public static final String FIELD_PRODUCT_GTIN = "additionalProperties.productGtin";
 
 }
