8eb1a96fefa50b22f8d7a0e04ccf7828dda97758
dalilaacqua <dacquavi@emeal.nttdata.com>
2025-11-19 17:03:09 +0100

    fix jUnit for MerchantTransaction

--- src/test/java/it/gov/pagopa/idpay/transactions/service/MerchantTransactionServiceImplTest.java
+++ src/test/java/it/gov/pagopa/idpay/transactions/service/MerchantTransactionServiceImplTest.java
@@ -28,103 +28,152 @@ import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.never;
 
 @ExtendWith(MockitoExtension.class)
 class MerchantTransactionServiceImplTest {
+
     @Mock
     private RewardTransactionRepository rewardTransactionRepository;
     @Mock
     private UserRestClient userRestClient;
+
     private MerchantTransactionService merchantTransactionService;
+
     private static final String INITIATIVE_ID = "INITIATIVEID1";
     private static final String MERCHANT_ID = "MERCHANTID1";
     private static final String USER_ID = "USERID1";
     private static final String FISCAL_CODE = "FISCALCODE1";
 
     @BeforeEach
-    void setUp(){
-        merchantTransactionService = new MerchantTransactionServiceImpl(userRestClient, rewardTransactionRepository);
+    void setUp() {
+        merchantTransactionService =
+                new MerchantTransactionServiceImpl(userRestClient, rewardTransactionRepository);
     }
 
     @Test
-    void getMerchantTransactionList() {
-        RewardTransaction rt1 = RewardTransactionFaker.mockInstanceBuilder(1)
+    void getMerchantTransactionListWithFiscalCode() {
+        RewardTransaction rewardTransaction = RewardTransactionFaker.mockInstanceBuilder(1)
                 .id("id1")
                 .amountCents(5000L)
                 .status("REWARDED")
                 .elaborationDateTime(LocalDateTime.now())
-                .rewards(getReward()).build();
-
-        MerchantTransactionDTO merchantTransaction1 = MerchantTransactionDTO.builder()
-                .trxId(rt1.getId())
-                .effectiveAmountCents(rt1.getAmountCents())
-                .rewardAmountCents(rt1.getRewards().get(INITIATIVE_ID).getAccruedRewardCents())
-                .fiscalCode(FISCAL_CODE)
-                .status(rt1.getStatus())
-                .elaborationDateTime(rt1.getElaborationDateTime())
-                .trxDate(rt1.getTrxDate())
-                .channel(rt1.getChannel())
+                .rewards(getReward())
                 .build();
 
-        MerchantTransactionsListDTO merchantTransactionsListDTO_expected = MerchantTransactionsListDTO.builder()
-                .content(List.of(merchantTransaction1))
-                .pageSize(10).totalElements(1).totalPages(1).build();
+        MerchantTransactionDTO merchantTransaction = MerchantTransactionDTO.builder()
+                .trxId(rewardTransaction.getId())
+                .effectiveAmountCents(rewardTransaction.getAmountCents())
+                .rewardAmountCents(rewardTransaction.getRewards().get(INITIATIVE_ID).getAccruedRewardCents())
+                .fiscalCode(FISCAL_CODE)
+                .status(rewardTransaction.getStatus())
+                .elaborationDateTime(rewardTransaction.getElaborationDateTime())
+                .trxDate(rewardTransaction.getTrxDate())
+                .channel(rewardTransaction.getChannel())
+                .build();
 
-        FiscalCodeInfoPDV fiscalCode = new FiscalCodeInfoPDV(USER_ID);
+        MerchantTransactionsListDTO merchantTransactionsListExpected = MerchantTransactionsListDTO.builder()
+                .content(List.of(merchantTransaction))
+                .pageSize(10)
+                .totalElements(1)
+                .totalPages(1)
+                .build();
+
+        FiscalCodeInfoPDV fiscalCodeInfo = new FiscalCodeInfoPDV(USER_ID);
         Pageable paging = PageRequest.of(0, 10, Sort.by(RewardTransaction.Fields.elaborationDateTime).descending());
 
-        Mockito.when(userRestClient.retrieveFiscalCodeInfo(anyString())).thenReturn(Mono.just(fiscalCode));
-        Mockito.when(rewardTransactionRepository.findByFilter(MERCHANT_ID, INITIATIVE_ID, USER_ID, null, paging)).thenReturn(Flux.just(rt1));
-        Mockito.when(rewardTransactionRepository.getCount(MERCHANT_ID, INITIATIVE_ID, null, null, USER_ID, null)).thenReturn(Mono.just(1L));
+        Mockito.when(userRestClient.retrieveFiscalCodeInfo(anyString()))
+                .thenReturn(Mono.just(fiscalCodeInfo));
 
-        Mono<MerchantTransactionsListDTO> resultMono = merchantTransactionService.getMerchantTransactions(MERCHANT_ID, INITIATIVE_ID, FISCAL_CODE, null, paging);
+        Mockito.when(rewardTransactionRepository.findByFilter(
+                        anyString(), anyString(), anyString(),
+                        any(), any(), any(), any(Pageable.class)))
+                .thenReturn(Flux.just(rewardTransaction));
+
+        Mockito.when(rewardTransactionRepository.getCount(
+                        anyString(), anyString(), any(), any(), any(), any()))
+                .thenReturn(Mono.just(1L));
+
+        Mono<MerchantTransactionsListDTO> resultMono =
+                merchantTransactionService.getMerchantTransactions(
+                        MERCHANT_ID,
+                        INITIATIVE_ID,
+                        FISCAL_CODE,
+                        null,
+                        null,
+                        null,
+                        paging
+                );
 
         MerchantTransactionsListDTO result = resultMono.block();
 
-        assertEquals(merchantTransactionsListDTO_expected, result);
+        assertEquals(merchantTransactionsListExpected, result);
+
+        Mockito.verify(userRestClient, never()).retrieveUserInfo(anyString());
     }
 
     @Test
-    void getMerchantTransactionListNoFiscalCode() {
-        RewardTransaction rt1 = RewardTransactionFaker.mockInstanceBuilder(1)
+    void getMerchantTransactionListWithoutFiscalCode() {
+        RewardTransaction rewardTransaction = RewardTransactionFaker.mockInstanceBuilder(1)
                 .id("id1")
                 .amountCents(5000L)
                 .status("REWARDED")
                 .elaborationDateTime(LocalDateTime.now())
-                .rewards(getReward()).build();
-
-        MerchantTransactionDTO merchantTransaction1 = MerchantTransactionDTO.builder()
-                .trxId(rt1.getId())
-                .effectiveAmountCents(rt1.getAmountCents())
-                .rewardAmountCents(rt1.getRewards().get(INITIATIVE_ID).getAccruedRewardCents())
-                .fiscalCode(FISCAL_CODE)
-                .status(rt1.getStatus())
-                .elaborationDateTime(rt1.getElaborationDateTime())
-                .trxDate(rt1.getTrxDate())
-                .channel(rt1.getChannel())
+                .rewards(getReward())
                 .build();
 
-        MerchantTransactionsListDTO merchantTransactionsListDTO_expected = MerchantTransactionsListDTO.builder()
-                .content(List.of(merchantTransaction1))
-                .pageSize(10).totalElements(1).totalPages(1).build();
+        MerchantTransactionDTO merchantTransaction = MerchantTransactionDTO.builder()
+                .trxId(rewardTransaction.getId())
+                .effectiveAmountCents(rewardTransaction.getAmountCents())
+                .rewardAmountCents(rewardTransaction.getRewards().get(INITIATIVE_ID).getAccruedRewardCents())
+                .fiscalCode(FISCAL_CODE)
+                .status(rewardTransaction.getStatus())
+                .elaborationDateTime(rewardTransaction.getElaborationDateTime())
+                .trxDate(rewardTransaction.getTrxDate())
+                .channel(rewardTransaction.getChannel())
+                .build();
 
-        UserInfoPDV userId = new UserInfoPDV(FISCAL_CODE);
+        MerchantTransactionsListDTO merchantTransactionsListExpected = MerchantTransactionsListDTO.builder()
+                .content(List.of(merchantTransaction))
+                .pageSize(10)
+                .totalElements(1)
+                .totalPages(1)
+                .build();
+
+        UserInfoPDV userInfo = new UserInfoPDV(FISCAL_CODE);
         Pageable paging = PageRequest.of(0, 10, Sort.by(RewardTransaction.Fields.elaborationDateTime).descending());
 
-        Mockito.when(userRestClient.retrieveUserInfo(anyString())).thenReturn(Mono.just(userId));
-        Mockito.when(rewardTransactionRepository.findByFilter(MERCHANT_ID, INITIATIVE_ID, null, null, paging)).thenReturn(Flux.just(rt1));
-        Mockito.when(rewardTransactionRepository.getCount(MERCHANT_ID, INITIATIVE_ID, null, null, null, null)).thenReturn(Mono.just(1L));
+        Mockito.when(userRestClient.retrieveUserInfo(anyString()))
+                .thenReturn(Mono.just(userInfo));
 
-        Mono<MerchantTransactionsListDTO> resultMono = merchantTransactionService.getMerchantTransactions(MERCHANT_ID, INITIATIVE_ID, null, null, paging);
+        Mockito.when(rewardTransactionRepository.findByFilter(
+                        anyString(), anyString(), isNull(),
+                        any(), any(), any(), any(Pageable.class)))
+                .thenReturn(Flux.just(rewardTransaction));
+
+        Mockito.when(rewardTransactionRepository.getCount(
+                        anyString(), anyString(), any(), any(), isNull(), any()))
+                .thenReturn(Mono.just(1L));
+
+        Mono<MerchantTransactionsListDTO> resultMono =
+                merchantTransactionService.getMerchantTransactions(
+                        MERCHANT_ID,
+                        INITIATIVE_ID,
+                        null,
+                        null,
+                        null,
+                        null,
+                        paging
+                );
 
         MerchantTransactionsListDTO result = resultMono.block();
 
-        assertEquals(merchantTransactionsListDTO_expected, result);
+        assertEquals(merchantTransactionsListExpected, result);
     }
 
     private static Map<String, Reward> getReward() {
-        Map<String, Reward> reward = new HashMap<>();
+        Map<String, Reward> rewardMap = new HashMap<>();
         RewardCounters counter = RewardCounters.builder()
                 .exhaustedBudget(false)
                 .initiativeBudgetCents(10000L)
@@ -141,8 +190,7 @@ class MerchantTransactionServiceImplTest {
                 .weeklyCapped(false)
                 .counters(counter)
                 .build();
-        reward.put(INITIATIVE_ID, rewardElement);
-        return reward;
+        rewardMap.put(INITIATIVE_ID, rewardElement);
+        return rewardMap;
     }
-
 }
