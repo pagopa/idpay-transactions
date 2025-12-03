cf9ddc56ab11d4e1df59ce52f0d62c979b45be39
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2025-12-03 15:13:30 +0100

    update assistance api to find transaction by initiativeId e not trxId

--- src/main/java/it/gov/pagopa/idpay/transactions/service/RewardTransactionService.java
+++ src/main/java/it/gov/pagopa/idpay/transactions/service/RewardTransactionService.java
@@ -9,7 +9,12 @@ import java.time.LocalDateTime;
 
 public interface RewardTransactionService {
     Mono<RewardTransaction> save(RewardTransaction rewardTransaction);
+
     Flux<RewardTransaction> findByIdTrxIssuer(String idTrxIssuer, String userId, LocalDateTime trxDateStart, LocalDateTime trxDateEnd, Long amountCents, Pageable pageable);
+
     Flux<RewardTransaction> findByRange(String userId, LocalDateTime trxDateStart, LocalDateTime trxDateEnd, Long amountCents, Pageable pageable);
+
     Mono<Void> assignInvoicedTransactionsToBatches(Integer chunkSize, Integer repetitionsNumber, boolean processAll, String trxId);
+
     Flux<RewardTransaction> findByInitiativeIdAndUserId(String initiativeId, String userId);
+}
