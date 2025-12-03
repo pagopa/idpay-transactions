0ef92ce26621e00e97a0d4b8d77ebaf9be345416
GiovanaSolorzano <giovanaelvira.solorzanochirieleison@emeal.nttdata.com>
2025-12-03 14:20:43 +0100

    [UPBE-451]  remove enable scheduler

--- src/main/java/it/gov/pagopa/idpay/transactions/IdpayTransactionsApplication.java
+++ src/main/java/it/gov/pagopa/idpay/transactions/IdpayTransactionsApplication.java
@@ -2,10 +2,8 @@ package it.gov.pagopa.idpay.transactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.cache.annotation.EnableCaching;
 
-@EnableScheduling
 @SpringBootApplication(scanBasePackages = "it.gov.pagopa")
 @EnableCaching
 public class IdpayTransactionsApplication {
