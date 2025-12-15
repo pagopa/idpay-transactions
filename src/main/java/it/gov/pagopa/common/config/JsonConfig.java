d7172643b0511298e4e99517ebff6f1dd7820ecb
EMORONGD6 <guido.moroni@emeal.nttdata.com>
2025-12-15 15:42:29 +0100

    feat: implement custom deserializer for OffsetDateTime and refactor ObjectMapper configuration

--- src/main/java/it/gov/pagopa/common/config/JsonConfig.java
+++ src/main/java/it/gov/pagopa/common/config/JsonConfig.java
@@ -7,35 +7,25 @@ import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.ser.OffsetDateTimeSerializer;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.OffsetDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.TimeZone;
 
 @Configuration
 public class JsonConfig {
 
+
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
-
-        JavaTimeModule javaTimeModule = new JavaTimeModule();
-        javaTimeModule.addSerializer(OffsetDateTime.class,
-            new OffsetDateTimeSerializer(
-                OffsetDateTimeSerializer.INSTANCE,
-                false,
-                DateTimeFormatter.ISO_OFFSET_DATE_TIME,
-                null
-            )
-        );
-
-        mapper.registerModule(javaTimeModule);
+        mapper.registerModule(configureDateTimeModule());
+        mapper.registerModule(new JavaTimeModule());
         mapper.registerModule(new Jdk8Module());
         mapper.registerModule(new ParameterNamesModule(JsonCreator.Mode.DEFAULT));
         mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
@@ -50,5 +40,10 @@ public class JsonConfig {
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         mapper.setTimeZone(TimeZone.getDefault());
         return mapper;
+    }
+
+    private static SimpleModule configureDateTimeModule() {
+        return new JavaTimeModule()
+                .addDeserializer(OffsetDateTime.class, new LocalDateTimeToOffsetDateTimeDeserializer());
     }
 }
