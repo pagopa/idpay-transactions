6315e5b15582a17c1f90952df70047e77e150946
EMORONGD6 <guido.moroni@emeal.nttdata.com>
2026-02-11 10:21:15 +0100

    refactor: simplify BatchCountersDTO by removing unnecessary annotations

--- src/main/java/it/gov/pagopa/idpay/transactions/dto/batch/BatchCountersDTO.java
+++ src/main/java/it/gov/pagopa/idpay/transactions/dto/batch/BatchCountersDTO.java
@@ -1,12 +1,8 @@
 package it.gov.pagopa.idpay.transactions.dto.batch;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
-@AllArgsConstructor
-@Builder
+@Getter
 public class BatchCountersDTO {
   private Long initialAmountCents;
   private Long numberOfTransactions;
