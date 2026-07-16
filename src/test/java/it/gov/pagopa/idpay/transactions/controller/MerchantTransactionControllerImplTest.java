dc2bc29aece0ec9bc5243a4ae78a9c6dff00d32b
BaldiVi-1 <vittorio.baldi@emeal.nttdata.com>
2026-07-16 17:43:23 +0200

    move getStatues from idpay-transaction to idpay-payment

--- src/test/java/it/gov/pagopa/idpay/transactions/controller/MerchantTransactionControllerImplTest.java
+++ src/test/java/it/gov/pagopa/idpay/transactions/controller/MerchantTransactionControllerImplTest.java
@@ -1,7 +1,6 @@
 package it.gov.pagopa.idpay.transactions.controller;
 
 import it.gov.pagopa.idpay.transactions.dto.MerchantTransactionsListDTO;
-import it.gov.pagopa.idpay.transactions.service.MerchantTransactionService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
